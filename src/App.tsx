7573df8bc7c8a74247101dc7b7231c305b2d37d6
yabets <yabets.g2@gmail.com>
2023-07-13 18:41:48 -0400

    Implement login page ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { LoginPage } from './components/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-     <h1 className="font-bold bg-red-500">test</h1>
-    </>
+   <LoginPage/>
   )
 }
 
